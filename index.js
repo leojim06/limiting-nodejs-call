99b046163e7e92886e3b5c5d8d84016f55c3a0d5
unknown <leojim06@gmail.com>
2021-07-06 21:58:03 -0500

    Adición de función y retorno de mensaje con código

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const PORT = 3000;
 
 const apiRequestLimiter = rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 2
+    max: 2,
+    handler: function(req, res) {
+        return res.status(429).json({
+            error: 'You sent too many requests. Plase wait a while then try again'
+        })
+    }
 });
 
 app.use(apiRequestLimiter);
