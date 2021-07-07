f3bbe4cd308e8823d0ac15462396603356b6109e
unknown <leojim06@gmail.com>
2021-07-06 21:51:01 -0500

    Adición limite de llamados

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const app = express();
 
 const PORT = 3000;
+
+const apiRequestLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 2
+});
+
+app.use(apiRequestLimiter);
 
 app.get('/', function (req, res) {
     return res.send('Hello world');
